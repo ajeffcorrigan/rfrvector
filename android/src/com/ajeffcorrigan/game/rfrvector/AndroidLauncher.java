6ad6acd10f9aa8269e03407f02f9a7e430214d7c
Jeff Corrigan <ajeffcorrigan@yahoo.com>
2016-09-20 14:00:00 -0300

    Initial commit

---
+++ android/src/com/ajeffcorrigan/game/rfrvector/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ajeffcorrigan.game.rfrvector;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ajeffcorrigan.game.rfrvector.RunFoxRunVector;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RunFoxRunVector(), config);
+	}
+}
---
+++ desktop/src/com/ajeffcorrigan/game/rfrvector/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ajeffcorrigan.game.rfrvector.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ajeffcorrigan.game.rfrvector.RunFoxRunVector;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunFoxRunVector(), config);
+	}
+}
