195b7ca59750ab0e7de0fbc3f2cb9e6c415328e2
ajeffcorrigan <ajeffcorrigan@yahoo.com>
2016-09-20 22:45:53 -0300

    First commit.

--- core/src/com/ajeffcorrigan/game/rfrvector/RunFoxRunVector.java
+++ core/src/com/ajeffcorrigan/game/rfrvector/RunFoxRunVector.java
@@ -1,33 +1,44 @@
 package com.ajeffcorrigan.game.rfrvector;
 
+import com.ajeffcorrigan.game.rfrvector.screens.PlayScreen;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class RunFoxRunVector extends ApplicationAdapter {
+public class RunFoxRunVector extends Game {
+
+	public static int gw;
+	public static int gh;
+	public static final boolean DEBUGON = true;			//Is debug enabled.
+	public static final float PTM = 100f;
+
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+
+		gw = Gdx.graphics.getWidth();
+		gh = Gdx.graphics.getHeight();
+
 		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+
 	}
 	
 	@Override
 	public void dispose () {
+		super.dispose();
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/ajeffcorrigan/game/rfrvector/screens/PlayScreen.java
@@ -0,0 +1,51 @@
+package com.ajeffcorrigan.game.rfrvector.screens;
+
+import com.ajeffcorrigan.game.rfrvector.RunFoxRunVector;
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by admin on 9/20/2016.
+ */
+
+public class PlayScreen implements Screen {
+
+    private RunFoxRunVector game;
+
+    public PlayScreen(RunFoxRunVector game) {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
