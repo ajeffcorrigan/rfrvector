4f158e912ee55dc06dbd4c2f96de666177817f35
Jeff Corrigan <ajeffcorrigan@yahoo.com>
2016-09-23 16:21:24 -0300

    Starting to implement Ashley Component Entity System framework.

---
+++ core/src/com/ajeffcorrigan/game/rfrvector/components/StateComponent.java
@@ -0,0 +1,11 @@
+package com.ajeffcorrigan.game.rfrvector.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by jacorrigan on 9/23/2016.
+ */
+
+public class StateComponent implements Component {
+
+}
